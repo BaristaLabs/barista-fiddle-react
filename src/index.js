96692aa7490e50a8a5e6c1447ab8ef00b0e64b85
Sean McLellan <sean.mclellan@sixconcepts.com>
2017-06-18 08:42:58 -0400

    Mess around with implementing app nav using different frameworks

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'font-awesome/css/font-awesome.css';
+import './cosmo/css/cosmo.css';
 import './index.css';
 
 injectTapEventPlugin();
--- src/workspace/Workspace.js
+++ src/workspace/Workspace.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
+import { Navbar, Nav, NavItem, ButtonToolbar, ButtonGroup } from 'react-bootstrap';
 import AppBar from 'material-ui/AppBar';
+import { Toolbar, ToolbarTitle, ToolbarGroup, ToolbarSeparator } from 'material-ui/Toolbar';
+import { CommandBar } from 'office-ui-fabric-react/lib/CommandBar';
+import FontIcon from 'material-ui/FontIcon';
+import FlatButton from 'material-ui/FlatButton';
+import IconButton from 'material-ui/IconButton';
+import MenuItem from 'material-ui/MenuItem';
 import { Tabs, Tab } from 'material-ui/Tabs';
 import SplitPane from '../split-pane/SplitPane.js';
 import JSEditor from './jsEditor/';
@@ -52,19 +59,70 @@ class Workspace extends Component {
     render() {
         return (
             <div>
-                {/*<Toolbar className="nav-bar">
+                <Toolbar className="fiddle-nav" style={{ backgroundColor: "black", justifyContent: "inherit" }}>
                     <ToolbarGroup firstChild={true}>
-                         <IconButton iconClassName="sidebar-toggle-button-icon" />
-                        <ToolbarTitle text="Barista Fiddle" />
-                        <ToolbarSeparator />
+                        <IconButton iconClassName="sidebar-toggle-button-icon" onTouchTap={this.toggleSidebar}></IconButton>
+                        <ToolbarTitle text="Barista Fiddle" style={{ color: "white" }} />
                     </ToolbarGroup>
-                </Toolbar>*/}
+                    <ToolbarGroup>
+                        <FlatButton
+                            label="Eval"
+                            labelPosition="after"
+                            icon={<FontIcon className="fa fa-play" style={{ fontSize: "inherit" }} />}
+                        />
+                    </ToolbarGroup>
+                </Toolbar>
+                <CommandBar
+        className="fiddle-nav2"
+          isSearchBoxVisible={ false }
+          items={ [ { propertyName: "foo", name: "asdf", value: "bar", icon: 'Add', onClick: this.toggleSidebar, style: { color: 'white' }}, { name: "Barista Fiddle" } ] }
+          farItems={ [] }
+        />
+                <nav className="navbar navbar-default fiddle-nav" role="navigation" style={{ marginBottom: 0, borderRadius: 0 }}>
+                    <div className="container-fluid">
+                        <div className="navbar-header">
+                            <a className="navbar-brand" href="#" onTouchTap={this.toggleSidebar}><span style={{ paddingRight: "10px" }}><i className="fa fa-bars"></i></span>Barista Fiddle</a>
+                        </div>
+                        <form className="navbar-form navbar-left">
+                            <div className="btn-group">
+                                <div type="button" className="btn btn-default" data-ng-click="evaluateScript(null, true)" data-ng-disable="model.errorCount > 0"><i className="fa fa-play"></i>&nbsp;Eval</div>
+                                <div type="button" className="btn btn-default" data-ng-click="debugScript()"><i className="fa fa-bug"></i>&nbsp;Debug</div>
+                            </div>
+                            <div className="btn-group">
+                                <div className="btn btn-default" data-ng-click="tidyUp()"><i className="fa fa-align-left"></i>&nbsp;TidyUp</div>
+                            </div>
+                        </form>
+                        <form className="navbar-form navbar-right">
+                            <div className="btn-group">
+                                <div className="btn btn-default" data-ng-click="showKeyboardShortcutsDialog()"><i className="fa fa-keyboard-o"></i></div>
+                            </div>
+                            <div className="btn-group">
+                                <div className="btn btn-default" data-ng-click="showGlobalSettingsDialog()"><i className="fa fa-cog"></i></div>
+                            </div>
+                        </form>
+                        <form className="navbar-form navbar-right">
+                            <div className="btn-group">
+                                <div className="btn btn-default" data-ng-class="{ 'btn-primary': isActiveTabShowingConsole() }" data-ng-click="toggleTabConsole()"><i className="fa fa-terminal"></i></div>
+                            </div>
+                        </form>
+                    </div>
+                </nav>
+                <nav className="fiddle-nav">
+                    <IconButton iconClassName="sidebar-toggle-button-icon" onTouchTap={this.toggleSidebar}></IconButton>
+                    <h1>Barista Fiddle</h1>
+                    <FlatButton
+                        label="Eval"
+                        labelPosition="after"
+                        icon={<FontIcon className="fa fa-play" style={{ fontSize: "inherit" }} />}
+                    />
+                </nav>
                 <AppBar
                     title="Barista Fiddle"
                     onLeftIconButtonTouchTap={this.toggleSidebar}
                     iconClassNameLeft="sidebar-toggle-button-icon"
                     style={{ zIndex: 0 }}
                 >
+                    <MenuItem onTouchTap={this.closeLeftNav} value={'/'} primaryText="Home" />
                 </AppBar>
                 <div id="workspace">
                     <SplitPane defaultSize="0" split="vertical"
